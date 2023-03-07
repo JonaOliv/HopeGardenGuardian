20321bd2b05affe047ef2ee49dabb7758a8aeffa
Jonathan Oliva <10662970+JonaOliv@users.noreply.github.com>
2023-03-07 18:30:06 +0800

    background fixing

--- Assets/Scripts/PlayScene/CameraHandler.cs
+++ Assets/Scripts/PlayScene/CameraHandler.cs
@@ -10,18 +10,55 @@ public class CameraHandler : MonoBehaviour
     public float yMin;
     public float yMax;
 
+    public float xOffset;
+    public float yOffset;
+
+    public float followSpeed;
+
+    public bool isXLocked;
+    public bool isYLocked;
+
+    public float X_DIFFERENCE = 15.0f;
+    public float Y_DIFFERENCE = 15.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        isXLocked = true;
+        isYLocked = false;
+
+        X_DIFFERENCE = Mathf.Abs(transform.position.x * 2 - Mathf.Clamp(player.transform.position.x + xOffset, xMin, xMax));
+        Y_DIFFERENCE = Mathf.Abs(transform.position.y * 2 - Mathf.Clamp(player.transform.position.y + yOffset, yMin, yMax));
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
-        float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
+        float x = Mathf.Clamp(player.transform.position.x + xOffset, xMin, xMax);
+        float y = Mathf.Clamp(player.transform.position.y + yOffset, yMin, yMax);
         Debug.Log("Camera y = " + y + ", x = " + x);
+
+        if (Mathf.Abs(transform.position.x - x) > X_DIFFERENCE)
+            isXLocked = false;
+
+        if (Mathf.Abs(transform.position.y - y) > Y_DIFFERENCE)
+            isYLocked = false;
+        
+        if (!isXLocked)
+        {
+            x = Mathf.Lerp(transform.position.x, x, Time.deltaTime * followSpeed);
+            isXLocked = true;
+        } else
+            x = transform.position.x;
+
+
+        if (!isYLocked)
+        {
+            y = Mathf.Lerp(transform.position.y, y, Time.deltaTime * followSpeed);
+            isXLocked = false;
+        } else
+            y = transform.position.y;
 
         transform.position = new Vector3(x, y, transform.position.z);
     }
